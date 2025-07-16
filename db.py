9ccc8c71fcfc7224c616c7b175503c0c7d7c763f
Ender Efe <efe.ender.er@gmail.com>
2025-07-16 15:44:21 +0300

    Database will be rewritten.

--- db.py
+++ db.py
@@ -18,50 +18,7 @@ Base = declarative_base()
 
 # --- Models --- #
 
-class MessageType(enum.Enum):
-    dm = "dm"
-    comment = "comment"
-
-class Client(Base):
-    __tablename__ = "clients"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=True)
-
-    messages = relationship("Message", back_populates="client")
-
-class Listing(Base):
-    __tablename__ = "listings"
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
-    price = Column(String)
-    features = Column(Text)
-
-    posts = relationship("Post", back_populates="listing")
-
-class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True)
-    listing_id = Column(Integer, ForeignKey("listings.id"))
-    post_url = Column(String, unique=True)
-
-    listing = relationship("Listing", back_populates="posts")
-    messages = relationship("Message", back_populates="post")
-
-class Message(Base):
-    __tablename__ = "messages"
-
-    id = Column(Integer, primary_key=True)
-    client_id = Column(Integer, ForeignKey("clients.id"))
-    post_id = Column(Integer, ForeignKey("posts.id"))
-    message_text = Column(Text, nullable=False)
-    response_text = Column(Text, nullable=True)
-    timestamp = Column(DateTime, default=datetime.utcnow)
-    message_type = Column(Enum(MessageType, name="message_type_enum"), nullable=False) 
-    response_sent = Column(Boolean, default=False)
-
-    client = relationship("Client", back_populates="messages")
-    post = relationship("Post", back_populates="messages")
-
+"""
+NEW DATABASE WILL BE CREATED
+LAST ONE WAS TOO MESSY AND I FORGOT WHAT I DID THINGS AND WHY I DID THOSE
+"""
